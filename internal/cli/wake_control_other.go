b32c14e7064e3078e7b78bee72293afe8e54ab83
avivsinai <aviv.s@taboola.com>
2026-07-21 16:20:26 +0300

    fix(wake): drop unused non-darwin cooperative-stop stub

--- internal/cli/wake_control_other.go
+++ internal/cli/wake_control_other.go
@@ -2,12 +2,7 @@
 
 package cli
 
-import "errors"
-
-var errNotSupported = errors.New("cooperative wake control is not supported on this platform")
-
 func wakeControlSocketPath(string, string, string) string { return "" }
 func startWakeControlListener(string, string, wakeLock) (func(), <-chan struct{}, func(), error) {
 	return func() {}, nil, func() {}, nil
 }
-func cooperativeStopInjectVia(wakeLockInspection) (bool, error) { return false, errNotSupported }
